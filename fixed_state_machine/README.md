43a35fe09c8cd07f763fa5ac95fde2b4ea0b18ef
Diego <marquez.diego.e@gmail.com>
2013-12-28 22:56:51 +0000

    Refactor fixed state machine example

--- fixed_state_machine/README.md
+++ fixed_state_machine/README.md
@@ -1,7 +1,10 @@
 # fixed-state-machine
 -------------------
 
-Insert some cool description here about why the hell you are doing this.
+An example on a different kind of state machine. The 'fixed state machine'. There is less comment rambling here.
+If you want to read that, go to the 'loose state machine' example.
+
+This example also illustrates how to separate state code into different files, if that is what rocks your boat.
 
 A [GAME-BUILDER][game-builder] project
 
--- fixed_state_machine/main.js
+++ fixed_state_machine/main.js
@@ -6,10 +6,34 @@ define(function(require){
 	var game = gb.game;
 	var root = gb.root;
 
-	
+	var stateMachineFactory = require('state_machine');
+
 	// This is the main initialization function
 	game.on("init", this, function() {
 		console.log("Welcome to Game-Builder!");
+
+		//This will create a loose state machine.
+		//By fixed I mean that the next and previous state of the current executing state
+		//are well defined and can not be bi-passed, like with a loose state machine.
+		//The good thing about this type of state machine is that it promotes less coupling
+		//between states.
+		var fixedStateMachine = stateMachineFactory.createFixedStateMachine(); 
+
+		//The states are now in different files. 
+		//Go check those out to see how they look.
+		var state_1 = require('../common_src/state_1');
+		var state_2 = require('../common_src/state_2');
+		var state_3 = require('../common_src/state_3');
+
+		//Note: After starting the state machine. The last and first states will be connected
+		// By the next() and previous() methods respectively.
+
+		fixedStateMachine.add(state_1('state_1_name'));
+		fixedStateMachine.add(state_2('state_2_name'));
+		fixedStateMachine.add(state_3('state_3_name'));
+
+		//Don't forget to start the state machine if you don't none of this nonesense will work.
+		fixedStateMachine.start('State machine was started!');
 	});
 
 	// This is called when the canvas looses focus
