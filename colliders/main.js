b4782490d6a44d49e9f93a406f9466656af9383a
Diego <marquez.diego.e@gmail.com>
2013-12-28 19:50:48 +0000

    Refactor colliders example

--- colliders/main.js
+++ colliders/main.js
@@ -6,10 +6,29 @@ define(function(require){
 	var game = gb.game;
 	var root = gb.root;
 
-	
+	game.add_extension(require('basic_layer_setup'));
+
 	// This is the main initialization function
 	game.on("init", this, function() {
 		console.log("Welcome to Game-Builder!");
+
+		require('colliders_bundle').create();
+
+		//This guy will be responsible for making everything work. And for setting collision pairs.
+		//More on that later
+		var collision_resolver = require('collision_resolver');
+
+		//Setting up collision pairs. The IDs used correspond to the ones in the colliders configuration.
+		//All colliders with ID 'Circle_1' will check for overlapping against all colliders with ID 'Polygon_1'
+		collision_resolver.addCollisionPair('Circle_Collider_ID', 'Polygon_Collider_ID');
+		//All colliders with ID 'Circle_1' will check for overlapping against all colliders with ID 'Fixed_Polygon_1'
+		collision_resolver.addCollisionPair('Circle_Collider_ID', 'Fixed_Polygon_Collider_ID');
+		//All colliders with ID 'Fixed_Polygon_1' will check for overlapping against all colliders with ID 'Polygon_1'
+		collision_resolver.addCollisionPair('Fixed_Polygon_Collider_ID', 'Polygon_Collider_ID');		
+
+		gb.addToLayer('Middle', 'Base_1');
+		gb.addToLayer('Middle', 'Base_2');
+		gb.addToLayer('Middle', 'Base_3');
 	});
 
 	// This is called when the canvas looses focus
