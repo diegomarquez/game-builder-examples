2adf320acf07a6859564f66862f1566480aea98f
Diego <marquez.diego.e@gmail.com>
2013-12-28 21:30:37 +0000

    Refactor keyboard example

--- common_src/bundles/input_bundle.js
+++ common_src/bundles/input_bundle.js
@@ -1,20 +1,22 @@
-gjs.setModulePath('basic_game_object');
-gjs.setModulePath('box_renderer');
-
 define(function(require) {
-	var Bundle = require('bundle');
-
 	var basic_game_object = require('basic_game_object'); 
 	var box_renderer = require('box_renderer');
 
-	var CollidersBundle = Bundle.extend({
+	var CollidersBundle = require('bundle').extend({
 		create: function() {
-			gjs.go_pool.createPool("Base", basic_game_object, 20);
-			gjs.co_pool.createPool("Box_Renderer", box_renderer, 20);
+			this.componentPool.createPool("Box_Renderer", box_renderer);
+			
+			this.componentPool.createConfiguration("Small_box", 'Box_Renderer')
+				.args({
+						offsetX: -10, 
+						offsetY: -10, 
+						width: 20, 
+						height: 20
+					});
 
-			gjs.co_pool.createConfiguration("Small_box", 'Box_Renderer').args({offsetX: -10, offsetY: -10, width: 20, height: 20});
-
-			gjs.go_pool.createConfiguration("Base_2", "Base")
+			this.gameObjectPool.createPool("Base", basic_game_object, 20);
+			
+			this.gameObjectPool.createConfiguration("Base_2", "Base")
 				.args({x: 200, y: 200, rotation_speed: 2})
 				.setRenderer('Small_box');
 		}
--- keyboard/main.js
+++ keyboard/main.js
@@ -5,11 +5,49 @@ define(function(require){
 	
 	var game = gb.game;
 	var root = gb.root;
-
+	var layers = gb.layers;
+	var assembler = gb.assembler;
+	var canvas = gb.canvas;
 	
+	var keyboard = require('keyboard');
+	var util = require('util');
+
+	game.add_extension(require('basic_layer_setup'));
+
 	// This is the main initialization function
 	game.on("init", this, function() {
 		console.log("Welcome to Game-Builder!");
+
+		require('input_bundle').create();
+
+		//Key Down Events
+		keyboard.onKeyDown(keyboard.A, this, function() { 
+			console.log("A was pressed");
+
+			// Since I want to do some stuff with this game object before displaying it
+			// I don't use the short hand method in the gb object
+			var go = assembler.get('Base_2');
+
+			go.x = util.rand_f(20, canvas.width-20);
+			go.y = util.rand_f(20, canvas.height-20);
+			//In this case I know that the renderer I am using has a 'color' property.
+			//If you write your own renderer this might not be available.
+			go.renderer.color = util.rand_color(); 
+
+			// Add and start the game object
+			layers.get('Middle').add(go).start();
+		});
+		
+		keyboard.onKeyDown(keyboard.S, this, function() { 
+			console.log("S was pressed") 
+		});
+		
+		keyboard.onKeyDown(keyboard.D, this, function() { console.log("D was pressed") });
+
+		//Key Up Events
+		keyboard.onKeyUp(keyboard.A, this, function() { console.log("A was released") });
+		keyboard.onKeyUp(keyboard.S, this, function() { console.log("S was released") });
+		keyboard.onKeyUp(keyboard.D, this, function() { console.log("D was released") });
 	});
 
 	// This is called when the canvas looses focus
@@ -28,6 +66,10 @@ define(function(require){
 		root.update(game.delta);
 		// Draws ALL the things.
 		root.transformAndDraw(game.context);
+
+		if(keyboard.isKeyDown(keyboard.A)){ console.log('A is down') }
+		if(keyboard.isKeyDown(keyboard.S)){ console.log('S is down') }
+		if(keyboard.isKeyDown(keyboard.D)){ console.log('D is down') }
 	});
 
 	// This is the main setup that kicks off the whole thing
