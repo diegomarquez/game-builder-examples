5fd9bf5cb9ba296fecde03bcc2b189cc935c78d5
Diego <marquez.diego.e@gmail.com>
2013-12-28 23:52:57 +0000

    Refactor reclaimer example

--- reclaimer/README.md
+++ reclaimer/README.md
@@ -1,7 +1,7 @@
 # reclaimer
 -------------------
 
-Insert some cool description here about why the hell you are doing this.
+This example uses the reclaimer object to reclaim all game objects of a certain type. Usefull for clearing the scene of all objects or just a set of them.
 
 A [GAME-BUILDER][game-builder] project
 
--- reclaimer/main.js
+++ reclaimer/main.js
@@ -5,11 +5,57 @@ define(function(require){
 	
 	var game = gb.game;
 	var root = gb.root;
+	var reclaimer = gb.reclaimer;
 
-	
+	game.add_extension(require('basic_layer_setup'));
+
+	var keyboard = require('keyboard');
+
 	// This is the main initialization function
 	game.on("init", this, function() {
 		console.log("Welcome to Game-Builder!");
+		
+		require('layering_bundle').create();
+
+		gb.addToLayer('Front', 'Base_1');
+		gb.addToLayer('Middle', 'Base_2');
+		gb.addToLayer('Back', 'Base_3');
+
+		//These are used to add back the stuff to the layers if you
+		//remove them while trying out the example.
+		keyboard.onKeyDown(keyboard.A, this, function() {
+			gb.addToLayer('Front', 'Base_1');
+			gb.addToLayer('Middle', 'Base_2');
+			gb.addToLayer('Back', 'Base_3');
+		});
+
+		keyboard.onKeyDown(keyboard.S, this, function() {
+			reclaimer.claimAll();
+		});
+
+		keyboard.onKeyDown(keyboard.Z, this, function() {
+			reclaimer.claimOnly('configuration', ['Base_2']);
+		});
+
+		keyboard.onKeyDown(keyboard.X, this, function() {
+			reclaimer.claimOnly('type', ['Base']);
+		});
+
+		keyboard.onKeyDown(keyboard.C, this, function() {
+			reclaimer.claimAllBut('configuration', ['Base_2']);
+		});
+
+		keyboard.onKeyDown(keyboard.P, this, function() {
+			reclaimer.clearAllPools();
+		});
+
+		keyboard.onKeyDown(keyboard.O, this, function() {
+			console.log( gb.goPool.toString() );
+		});
+
+		keyboard.onKeyDown(keyboard.I, this, function() {
+			console.log( gb.coPool.toString() );
+		});
 	});
 
 	// This is called when the canvas looses focus
