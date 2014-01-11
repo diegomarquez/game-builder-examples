cba4bd6f4561a96d62e0d8fd9818f28e9e1bc9d7
Diego <marquez.diego.e@gmail.com>
2014-01-11 23:23:07 +0000

    More little fixes to the docs. /n/n Pushed by grunt on 2014-01-11

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,20 +3,36 @@ module.exports = function(grunt) {
     'examples/colliders/Gruntfile.js', 
     'examples/empty/Gruntfile.js',
     'examples/extensions/Gruntfile.js',
-    'examples/fixed_state-machine/Gruntfile.js',
-    'examples/game-object_creation/Gruntfile.js',
-    'examples/game-object_nesting/Gruntfile.js',
+    'examples/fixed_state_machine/Gruntfile.js',
+    'examples/game_object_creation/Gruntfile.js',
+    'examples/game_object_nesting/Gruntfile.js',
     'examples/keyboard/Gruntfile.js',
     'examples/layering/Gruntfile.js',
     'examples/logic_components/Gruntfile.js',
-    'examples/loose_state-machine/Gruntfile.js',
+    'examples/loose_state_machine/Gruntfile.js',
     'examples/reclaimer/Gruntfile.js',
     'examples/renderers/Gruntfile.js',
     'examples/sound/Gruntfile.js',
     'examples/timers/Gruntfile.js'
   ];
 
+  var checkValidCommitMessage = function() {
+    if(!grunt.option('message')) {
+      grunt.fail.fatal('Missing commit message');  
+    }
+
+    if(grunt.util.kindOf(grunt.option('message')) != 'string') {
+      grunt.fail.fatal('Missing commit message'); 
+    }
+
+    if(grunt.option('message').length < 10) {
+      grunt.fail.fatal('Commit message is too short. Must be greater than 10 characters');  
+    }
+  };
+
   grunt.initConfig({
+    commitMessage: grunt.option('message'),
+
     hub: {
       refreshAll: {
         src: files,
@@ -26,10 +42,41 @@ module.exports = function(grunt) {
           concurrent: files.length
         }
       }
-    }
+    },
+
+    shell: {
+      pushExamples: {
+        command: function() {
+          checkValidCommitMessage();
+
+          return [ 
+            'git add . -A',
+            'git diff --quiet --exit-code --cached || git commit -m "<%= commitMessage %>. /n/n Pushed by grunt on <%= grunt.template.today("yyyy-mm-dd") %>"',
+            'git push -f'
+          ].join('&&');   
+        }
+      },
+
+      pushGameBuilder: {
+        command: function() {
+          checkValidCommitMessage();
+
+          return [
+            'cd game-builder',
+            'git add . -A',
+            'git diff --quiet --exit-code --cached || git commit -m "<%= commitMessage %>. /n/n Pushed by grunt on <%= grunt.template.today("yyyy-mm-dd") %>"',
+            'git push -f',
+            'cd ..'
+          ].join('&&');
+        }
+      }    
+    } 
   });
 
   grunt.loadNpmTasks('grunt-hub');
+  grunt.loadNpmTasks('grunt-shell');
 
   grunt.registerTask('default', ['hub:refreshAll']);
+
+  grunt.registerTask('push', ['shell:pushExamples', 'shell:pushGameBuilder']);
 };
