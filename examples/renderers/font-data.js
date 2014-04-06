45a248da3784b236e3fb185d7f52899ff4021768
Diego <marquez.diego.e@gmail.com>
2014-04-06 16:43:39 +0100

    Send font loading configuration in single object to font-loader.js

--- examples/renderers/font-data.js
+++ examples/renderers/font-data.js
@@ -8,5 +8,8 @@ var webfonts = {
 
 	// Set a version of [Web font loader](https://github.com/typekit/webfontloader) to download from 
 	// [Google](https://developers.google.com/speed/libraries/)
-	version: '1.5.2'
+	version: '1.5.2',
+
+	// Set this to false if you don't want to load any external fonts with the [Web font loader](https://github.com/typekit/webfontloader)
+	loadFonts: true
 }
