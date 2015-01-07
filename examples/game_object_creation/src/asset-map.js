461996b47ee73eef9b59527932059864134e653f
Diego <marquez.diego.e@gmail.com>
2015-01-07 20:46:12 +0000

    Update game-object-creation example grunt tasks

--- examples/game_object_creation/src/asset-map.js
+++ examples/game_object_creation/src/asset-map.js
@@ -7,7 +7,13 @@ define(function() {
 	"CROW.WAV": "../common_assets/sound/crow.wav",
 	"ELEVATOR.MP3": "../common_assets/sound/elevator.mp3",
 	"HORSE.MP3": "../common_assets/sound/horse.mp3",
-	"SHEEP.MP3": "../common_assets/sound/sheep.mp3"
+	"SHEEP.MP3": "../common_assets/sound/sheep.mp3",
+	"TEST.JPG": "assets/images/test.jpg",
+	"TEST.MP3": "assets/sound/test.mp3",
+	"TEST.WAV": "assets/sound/test.wav",
+	"TEST2.MP3": "assets/sound/test2.mp3",
+	"TEST3.MP3": "assets/sound/test3.mp3",
+	"TEST4.MP3": "assets/sound/test4.mp3"
 };
 
 	Data.prototype.get = function() {
