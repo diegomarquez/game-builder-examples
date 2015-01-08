9b0af3aa9b9cce22e17096f1c42d3ebc151a1832
Diego <marquez.diego.e@gmail.com>
2015-01-08 13:10:37 +0000

    Update reclaimer and renderers examples

--- examples/renderers/src/font-data.js
+++ examples/renderers/src/font-data.js
@@ -1,8 +1,11 @@
-var webfonts = {
+define(function() {
+	var Data = function() {};
+
+	var data = {
 	// Refer [Web font loader](https://github.com/typekit/webfontloader) for details on how it shoulf look like.
 	data: {
 		google: {
-			families: []
+			families: ['Audiowide', 'Gafata', 'Miltonian Tattoo']
 		},
 	},
 
@@ -11,5 +14,12 @@ var webfonts = {
 	version: '1.5.2',
 
 	// Set this to false if you don't want to load any external fonts with the [Web font loader](https://github.com/typekit/webfontloader)
-	loadFonts: false
-}
+	loadFonts: true
+};
+
+	Data.prototype.get = function() {
+		return data;
+	}
+
+	return new Data();
+});
